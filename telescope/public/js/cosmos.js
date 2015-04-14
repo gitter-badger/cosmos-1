799ea6ff651a484197ac22739519559c13cbdceb
brann <brann@cosmos.io>
2015-04-14 18:54:29 +0900

    dynamic page change

--- telescope/public/js/cosmos.js
+++ telescope/public/js/cosmos.js
@@ -1,21 +1,23 @@
 (function() {
     // Route configuration
     $(function() {
-        Route.match('/', function() {
-        });
-        Route.match('/planets', function() {
-        });
-        Route.match('/containers', function() {
-        });
+        Route.match('/', function() {});
+        Route.match('/planets', function() {});
+        Route.match('/containers', function() {});
         Route.defaultRoute = function() {
             window.location.replace("/");
         };
         Route.regist();
     });
 
-
-    window.onpopstate = history.onpushstate = function(e) { 
-        console.log(e);
-    };
-
+    $(window).on('pushstate', function(e, param) {
+        if (param) {
+            console.log(param.page);
+        }
+    });
+    window.addEventListener('popstate', function(e) {
+        if (e.state) {
+            console.log(e.state.page);
+        }
+    });
 }());
