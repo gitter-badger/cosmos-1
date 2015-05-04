5e1ca338f8fb057bbf3edd9890001bbdf77ec787
Graham Lee <ghmlee@ghmlee.com>
2015-05-04 20:27:12 +0900

    influxdb client is added.

--- main.go
+++ main.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-	"os"
-	"fmt"
+    "os"
+    "fmt"
     "path"
-	"strings"
-	"io/ioutil"
-	"net/http"
-	"encoding/json"
+    "strings"
+    "io/ioutil"
+    "net/http"
+    "encoding/json"
 
     "github.com/cosmos-io/cosmos/context"
-	"github.com/cosmos-io/cosmos/dao"
-	"github.com/cosmos-io/cosmos/router"
-	"github.com/cosmos-io/cosmos/service"
-	"github.com/cosmos-io/cosmos/worker"
-	"github.com/cosmos-io/influxdbc"
-    
+    "github.com/cosmos-io/cosmos/dao"
+    "github.com/cosmos-io/cosmos/router"
+    "github.com/cosmos-io/cosmos/service"
+    "github.com/cosmos-io/cosmos/worker"
+    "github.com/cosmos-io/influxdbc"
+
     "github.com/gorilla/mux"
 )
 
 var (
-	cosmosPort    = getEnv("COSMOS_PORT", "8888")
+    cosmosPort    = getEnv("COSMOS_PORT", "8888")
 	dbHost        = getEnv("INFLUXDB_HOST", "localhost")
 	dbPort        = getEnv("INFLUXDB_PORT", "8086")
 	dbUsername    = getEnv("INFLUXDB_USERNAME", "root")
@@ -43,7 +43,7 @@ func getEnv(key, defaultValue string) string {
 }
 
 // A middleware to distingush text/html content type and others
-// 
+//
 func serveIndexHTML(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		accept := strings.ToLower(r.Header.Get("Accept"))
@@ -87,14 +87,14 @@ func createInfluxDBClient() (*influxdbc.InfluxDB, error) {
 	if err != nil {
         return db, err
 	}
-    
+
 	_, err = db.CreateDatabase(conf)
 	if err != nil {
         return db, err
 	} else {
         fmt.Println("[InfluxDB] A database is created.")
     }
-    
+
 	return db, nil
 }
 
@@ -102,30 +102,33 @@ func createInfluxDBClient() (*influxdbc.InfluxDB, error) {
 //
 func run() {
     publicPath := path.Join("telescope", "public")
-    
+
     mux := mux.NewRouter()
-    
+
     mux.HandleFunc("/v1/newsfeeds",
         serveContext(router.GetNewsFeeds)).Methods("GET")
-    
+
     mux.HandleFunc("/v1/planets/{planet}/containers/{container}",
         serveContext(router.GetContainerMetrics)).Methods("GET")
-    
+
     mux.HandleFunc("/v1/planets/{planet}/containers",
         serveContext(router.GetContainersOfPlanet)).Methods("GET")
-    
+
     mux.HandleFunc("/v1/planets/{planet}",
         serveContext(router.GetPlanetMetrics)).Methods("GET")
-    
+
     mux.HandleFunc("/v1/planets",
         serveContext(router.GetPlanets)).Methods("GET")
-    
+
     mux.HandleFunc("/v1/containers",
         serveContext(router.GetContainers)).Methods("GET")
-    
+
+    mux.HandleFunc("/v1/planets/{planet}/containers/{container}",
+        serveContext(router.AddContainer)).Methods("POST")
+
     mux.HandleFunc("/v1/planets/{planet}/containers",
         serveContext(router.AddContainersOfPlanet)).Methods("POST")
-    
+
     mux.PathPrefix("/").Handler(http.FileServer(http.Dir(publicPath)))
 
     middlewares := serveIndexHTML(mux)
@@ -140,7 +143,7 @@ func init() {
         fmt.Println(err.Error())
     }
     dao.Initialize(db)
-    
+
 	newsFeedWorker.Run()
 }
 
